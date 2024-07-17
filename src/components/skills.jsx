e14aa68800f41d574d3cf44995764e12854ce871
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-07-17 06:11:59 -0600

    Update assets

--- src/components/skills.jsx
+++ src/components/skills.jsx
@@ -5,17 +5,17 @@ function Skills() {
     <div className="skills-section">
       <h3>Skills</h3>
       <div className="skill-list">
-        <img src="https://w7.pngwing.com/pngs/5/56/png-transparent-website-development-html5-logo-world-wide-web-consortium-world-wide-web-angle-web-design-text-thumbnail.png" alt="HTML" />
-        <img src="/src/assets/img/js.png" alt="JavaScript" />
-        <img src="/src/assets/img/boots.png" alt="Bootstrap" />
-        <img src="/src/assets/img/react.png" alt="React" />
-        <img src="/src/assets/img/nodejs.png" alt="Node.js" />
-        <img src="/src/assets/img/MySql.png" alt="MySQL" />
-        <img src="/src/assets/img/github.png" alt="GitHub" />
-        <img src="/src/assets/img/git.png" alt="Git" />
-        <img src="/src/assets/img/mongodb.png" alt="MongoDB" />
-        <img src="/src/assets/img/css-3.png" alt="CSS" />
-        <img src="/src/assets/img/sqlS.png" alt="SQL Server" />
+        <img src="https://cdn.iconscout.com/icon/free/png-256/free-html5-41-1175209.png?f=webp&w=256" alt="HTML" />
+        <img src="https://cdn.iconscout.com/icon/free/png-256/free-css3-9-1175237.png?f=webp&w=256" alt="JavaScript" />
+        <img src="https://dh.virginia.edu/system/files/styles/large/private/bootstrap-stack.png?itok=b_S8F9nO" alt="Bootstrap" />
+        <img src="https://www.svgrepo.com/show/358128/react.svg" alt="React" />
+        <img src="https://static-00.iconduck.com/assets.00/node-js-icon-454x512-nztofx17.png" alt="Node.js" />
+        <img src="https://cdn.iconscout.com/icon/free/png-256/free-mysql-21-1174941.png" alt="MySQL" />
+        <img src="https://cdn-icons-png.flaticon.com/512/25/25231.png" alt="GitHub" />
+        <img src="https://git-scm.com/images/logos/downloads/Git-Icon-1788C.png" alt="Git" />
+        <img src="https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_wordmark_logo_icon_146425.png" alt="MongoDB" />
+        <img src="https://cdn-icons-png.flaticon.com/512/5968/5968292.png" alt="CSS" />
+        <img src="https://seeklogo.com/images/A/astro-icon-logo-44253BACEE-seeklogo.com.png" alt="Astro" />
       </div>
     </div>
   );
