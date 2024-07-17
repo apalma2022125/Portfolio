b91b68b4de15abd99a25c1c9d17f70df77ce07a1
apalma2022125 <apalma-2022125@kinal.edu.gt>
2024-07-17 09:36:24 -0600

    Update assets

--- src/Pages/trayectoriaEducativa.jsx
+++ src/Pages/trayectoriaEducativa.jsx
@@ -1,22 +1,44 @@
 import React from 'react';
 
-function Certification() {
+function EducationalPath() {
   return (
-    <section id="certificados">
-      <h2>Trayectoria Educativa</h2>
-      <div className="education-container">
-        <div className="education-image"></div>
-        <div className="education-details">
-          <h3>Instituciones y grados</h3>
-          <ul>
-            <li><strong>Educación primaria</strong> (2011-2016) - Centro Educativo Bilingue La Vid</li>
-            <li><strong>Educación básica</strong> (2017-2021) - Colegio Fuente de Vida</li>
-            <li><strong>Diversificado: Perito en Computación</strong> (2022-2024) - Centro Educativo Técnico Laboral KINAL</li>
-          </ul>
+    <section id="educational-path">
+      <h2 className="title">Trayectoria Educativa</h2>
+      <div className="container">
+        <div className="info">
+          <div className="education-item">
+            <h3>Educación Primaria</h3>
+            <p>(2009-2014) - Centro Educativo Preceptos</p>
+          </div>
+          <div className="education-item">
+            <h3>Educación Básica</h3>
+            <p>(2015-2017) - Centro Educativo Técnico Laboral KINAL</p>
+          </div>
+          <div className="education-item">
+            <h3>Diversificado: Perito en Computación</h3>
+            <p>(2018-2020) - Centro Educativo Técnico Laboral KINAL</p>
+          </div>
+        </div>
+        <div className="image"></div>
+      </div>
+      <div className="container">
+        <div className="image-diversificado"></div>
+        <div className="info">
+          <h3>4to. Diversificado</h3>
+          <p>
+            Durante este grado principalmente desarrollé conceptos básicos de <strong>IT Essentials</strong>,
+            configuración de redes a través de los cursos <strong>CCNA 1 y 2</strong> de Cisco, así como también
+            aprendí principios básicos de programación y a desarrollar en el lenguaje <strong>Java</strong> ejecutado
+            desde la CLI.
+          </p>
+          <p>
+            Mis prácticas de grado las realicé en el área de tecnología en la <strong>Municipalidad</strong> de Villa
+            Nueva, trabajando principalmente en el área de soporte técnico y mantenimiento de equipo.
+          </p>
         </div>
       </div>
     </section>
   );
 }
 
-export default Certification;
+export default EducationalPath;
